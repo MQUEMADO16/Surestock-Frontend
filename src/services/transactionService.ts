765f346c129e959434eebeedc0a96cf420cf0cf3
Miguel Quemado <mquemado16@gmail.com>
2025-12-06 20:57:40 -0500

    Added getHistory endpoint to transaction service

--- src/services/transactionService.ts
+++ src/services/transactionService.ts
@@ -6,6 +6,12 @@ const transactionService = {
   // Process a sale (Checkout)
   createTransaction: async (data: SaleRequest) => {
     return api.post<SalesTransaction[]>('/transactions', data);
+  },
+
+  getHistory: async () => {
+    var history = api.get<SalesTransaction[]>('/transactions');
+    console.log(history);
+    return history;
   }
 };
 
